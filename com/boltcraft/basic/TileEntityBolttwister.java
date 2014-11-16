792df4e8962d84e80d6d1b96c80ab225e5fa4b6f
xSenseyex <xSenseyex@yandex.ru>
2014-11-17 02:48:40 +0300

    Lol

--- com/boltcraft/basic/TileEntityBolttwister.java
+++ com/boltcraft/basic/TileEntityBolttwister.java
@@ -3,5 +3,31 @@ package com.boltcraft.basic;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityBolttwister extends TileEntity {
+	
+	 int tick = 0;
+	 
+	    public static final String publicName = "tileEntityBolttwister";
+	    private String name = "tileEntityBolttwister";
+	 
+	    public String getName() {
+	 
+	        return name;
+	    }
+	 
+	    @Override
+	    public void updateEntity() {
+	 
+	        if(!worldObj.isRemote) {
+	 
+	            tick++;
+	            if(tick == 100) {
+	 
+	                this.worldObj.setWorldTime(1000);
+	                tick = 0;
+	            }
+	        }
+	    }
+	}
+	
 
-}
+
