966fa41bd96f07431344df57f83558695adb3ea7
MarcusXDead <ruty2@mail.ru>
2014-11-16 20:44:50 +0300

    Adding core
    Core folders

---
+++ com/boltcraft/basic/BoltBlock.java
@@ -0,0 +1,13 @@
+package com.boltcraft.basic;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BoltBlock extends Block {
+
+	public BoltBlock() {
+		super(Material.iron);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ com/boltcraft/basic/Boltcraft.java
@@ -0,0 +1,52 @@
+package com.boltcraft.basic;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
+import com.example.examplemod.ExampleMod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@Mod(modid = Boltcraft.MODID, version = Boltcraft.VERSION)
+
+public class Boltcraft
+{
+    public static final String MODID = "Boltcraft";
+    public static final String VERSION = "0.1";
+    
+    
+    public static CreativeTabs bolttab = new CreativeTabs("Boltcraft"){
+    	public Item getTabIconItem(){
+    		return Items.arrow;
+    	}
+    };
+    // items
+    public static Item boltiron;
+    
+    
+    
+    // blocks
+    public static Block boltblock;
+    
+    
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	//items    
+        boltiron = new Item().setUnlocalizedName("ironBolt").setCreativeTab(bolttab).setTextureName(MODID + ":" + "default");
+        GameRegistry.registerItem(boltiron, "ironBolt");
+        
+        
+        //blocks
+        
+        boltblock = new BoltBlock().setBlockName("boltBlock").setCreativeTab(bolttab).setBlockTextureName("dirt");
+        GameRegistry.registerBlock(boltblock, "blockBlock");
+    }
+}
