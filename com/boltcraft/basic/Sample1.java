987b4fc88bcd427fc866620fbb160b61f6028c9a
MarcusXDead <ruty2@mail.ru>
2014-11-17 02:32:19 +0300

    Sample

---
+++ com/boltcraft/basic/Sample1.java
@@ -0,0 +1,58 @@
+package com.boltcraft.basic;
+import java.util.Random;
+
+import net.minecraft.world.World;
+public class Sample1 {
+public boolean generate(World world, Random par2Random, int 0, int 0, int 0) {
+      world.setBlock(0 + 0, 0 + 0, 0 + 0, 4, 0, 1);
+      world.setBlock(0 + 1, 0 + 0, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 0, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 0, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 0, 0 + 1, 4, 0, 1);
+      world.setBlock(0 + 1, 0 + 0, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 0, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 0, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 0, 0 + 2, 4, 0, 1);
+      world.setBlock(0 + 1, 0 + 0, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 0, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 0, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 0, 0 + 3, 4, 0, 1);
+      world.setBlock(0 + 1, 0 + 0, 0 + 3, 4, 0, 1);
+      world.setBlock(0 + 2, 0 + 0, 0 + 3, 4, 0, 1);
+      world.setBlock(0 + 3, 0 + 0, 0 + 3, 4, 0, 1);
+      world.setBlock(0 + 0, 0 + 1, 0 + 0, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 1, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 1, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 1, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 1, 0 + 1, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 1, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 1, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 1, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 1, 0 + 2, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 1, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 1, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 1, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 1, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 1, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 2, 0 + 1, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 3, 0 + 1, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 0, 0 + 2, 0 + 0, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 2, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 2, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 2, 0 + 0, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 2, 0 + 1, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 2, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 2, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 2, 0 + 1, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 2, 0 + 2, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 2, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 2, 0 + 2, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 3, 0 + 2, 0 + 2, 5, 0, 1);
+      world.setBlock(0 + 0, 0 + 2, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 1, 0 + 2, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 2, 0 + 2, 0 + 3, 0, 0, 1);
+      world.setBlock(0 + 3, 0 + 2, 0 + 3, 0, 0, 1);
+return true;
+  }
+}
+
