84fd249f268ad3b68a3a3f99aab81f67fac2d1d1
MarcusXDead <ruty2@mail.ru>
2014-11-16 21:57:26 +0300

    version update

--- com/boltcraft/basic/Boltcraft.java
+++ com/boltcraft/basic/Boltcraft.java
@@ -17,7 +17,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class Boltcraft
 {
     public static final String MODID = "Boltcraft";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     
     
     public static CreativeTabs bolttab = new CreativeTabs("Boltcraft"){
